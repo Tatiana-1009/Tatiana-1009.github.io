77759ecd6ca12e28639b548ac0c5c4d6006f4328
Tatiana-1009 <ts10091990@gmail.com>
2025-07-17 21:59:50 +0300

    Test_Recipe.py

--- tests/Test_Recipe.py
+++ tests/Test_Recipe.py
@@ -1,31 +1,45 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-def test_recipe():
+def test_recipe01():
     driver = webdriver.Chrome()
     driver.get("https://tatiana-1009.github.io/Recipe.html")
-
     # Название блюда (тег h1), текст: "Простой бутерброд с сыром":
-    assert "Простой бутерброд с сыром" in driver.find_element(By.TAG_NAME, "h1").text
+    assert driver.find_element(By.TAG_NAME, "h1").text == "Простой бутерброд с сыром"
+    driver.quit()
 
+def test_recipe02():
+    driver = webdriver.Chrome()
+    driver.get("https://tatiana-1009.github.io/Recipe.html")
     # Раздел "Что нужно":
-    assert "Что нужно" in driver.find_element(By.TAG_NAME, "h2").text
+    assert driver.find_element(By.TAG_NAME, "h2").text == "Что нужно"
+    driver.quit()
 
+def test_recipe03():
+    driver = webdriver.Chrome()
+    driver.get("https://tatiana-1009.github.io/Recipe.html")
     # В абзаце (тег p) перечислите ингредиенты через запятую
     # "Хлеб, сливочное масло, сыр, зелень (по желанию)":
-    assert ("Хлеб, сливочное масло, сыр, зелень (по желанию)"
-            in driver.find_element(By.TAG_NAME, "body").text)
+    assert driver.find_element(By.TAG_NAME, "p").text == "Хлеб, сливочное масло, сыр, зелень (по желанию)"
+    driver.quit()
 
+def test_recipe04():
+    driver = webdriver.Chrome()
+    driver.get("https://tatiana-1009.github.io/Recipe.html")
     # Раздел "Как приготовить" (тег h2):
-    assert "Как приготовить" in driver.find_element(By.TAG_NAME, "body").text
+    assert driver.find_element(By.CSS_SELECTOR, "body > h2:nth-child(4)").text == "Как приготовить"
+    driver.quit()
 
+def test_recipe05():
+    driver = webdriver.Chrome()
+    driver.get("https://tatiana-1009.github.io/Recipe.html")
     # Разбейте инструкцию на отдельные шаги, каждый в своём абзаце (p):
-    # "Намажьте хлеб маслом."
-    # "Положите сверху ломтик сыра."
-    # "Добавьте зелень, если хотите."
+    # "Намажьте хлеб маслом"
+    # "Положите сверху ломтик сыра"
+    # "Добавьте зелень, если хотите"
     # "Бутерброд готов!"
-    assert ("Намажьте хлеб маслом, Положите сверху ломтик сыра,"
-            "Добавьте зелень, если хотите, Бутерброд готов!"
-            in driver.find_element(By.TAG_NAME, "body").text)
-
+    assert driver.find_element(By.CSS_SELECTOR, "body > p:nth-child(5)").text == "Намажьте хлеб маслом"
+    assert driver.find_element(By.CSS_SELECTOR, "body > p:nth-child(6)").text == "Положите сверху ломтик сыра"
+    assert driver.find_element(By.CSS_SELECTOR, "body > p:nth-child(7)").text == "Добавьте зелень, если хотите"
+    assert driver.find_element(By.CSS_SELECTOR, "body > p:nth-child(8)").text == "Бутерброд готов!"
     driver.quit()
